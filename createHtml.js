c610f1443ad79cea8f0e920690571bf3ff3cbdbb
swapnil lothe <44019530+swapnillothe@users.noreply.github.com>
2018-12-28 16:29:53 +0530

    Add one person test.

---
+++ createHtml.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let htmlFile = '<!DOCTYPE html>\n<html>\n<h3>sauron_reports</h3>\n'
+htmlFile += '<body>\n<table>\n<tr>No. </tr>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n'
+htmlFile += '<tr>username </tr>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n'
+htmlFile += '<tr>shaId </tr>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n<tr>pass </tr>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n<tr>time </tr>\n'
